d10a5d8c881c140fafa742dbf24898484f1d3078
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-26 19:17:15 +0200

    Update select.mjs

--- backend/select.mjs
+++ backend/select.mjs
@@ -9,17 +9,19 @@ const dcp = "C:\\RCOC\\default_cursors";
 const tbl = {
     "Reset Cursors": {
         execute: async () => {
-            if (!fs.existsSync(`${dcp}\\arrow.png`) || !fs.existsSync(`${dcp}\\arrowfar.png`) || !fs.existsSync(`${dcp}\\ibeam.png`)) {
+            if (!fs.existsSync(`${dcp}\\arrow.png`) || !fs.existsSync(`${dcp}\\arrowfar.png`) || !fs.existsSync(`${dcp}\\ibeam.png` || !fs.existsSync(`${dcp}\\shiftlock.png`))) {
                 const dc = await utils.getDefaultCursors();
                 if (!fs.existsSync(dcp)) fs.mkdirSync(dcp);
                 fs.writeFileSync(`${dcp}\\arrow.png`, dc.arrow);
                 fs.writeFileSync(`${dcp}\\arrowfar.png`, dc.arrowfar);
                 fs.writeFileSync(`${dcp}\\ibeam.png`, dc.ibeam);
+                fs.writeFileSync(`${dcp}\\shiftlock.png`, dc.shiftlock);
             }
             fs.writeFileSync("C:\\RCOC\\selected.json", JSON.stringify({
                 "ArrowFar": `${dcp}\\arrowfar.png`,
                 "Arrow": `${dcp}\\arrow.png`,
-                "IBeam": `${dcp}\\ibeam.png`
+                "IBeam": `${dcp}\\ibeam.png`,
+                "ShiftLock": `${dcp}\\shiftlock.png`
             }));
             askMode();
         }
@@ -86,6 +88,28 @@ const tbl = {
                 })
                 const currentActive = JSON.parse(fs.readFileSync("C:\\RCOC\\selected.json"));
                 currentActive["IBeam"] = paths[0];
+                fs.writeFileSync("C:\\RCOC\\selected.json", JSON.stringify(currentActive));
+            } catch {}
+            askMode();
+        }
+    },
+    "Change Shift Lock Cursor": {
+        execute: async () => {
+            try {
+                const paths = await dialog({
+                    dialogtype: 'open-file',
+                    types: [
+                        {
+                            display: "all image files", extensions: "*.png;*.jpg;*.jpeg;*.gif;*.bmp;*.ico"
+                        },
+                        {
+                            display: "all files", extensions: "*.*"
+                        }
+                    ],
+                    title: "pick a cursor to use"
+                })
+                const currentActive = JSON.parse(fs.readFileSync("C:\\RCOC\\selected.json"));
+                currentActive["ShiftLock"] = paths[0];
                 fs.writeFileSync("C:\\RCOC\\selected.json", JSON.stringify(currentActive));
             } catch {}
             askMode();
