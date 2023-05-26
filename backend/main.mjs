6e0f3d18bddfcba41829f960be5dee9bc7ad023d
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-26 19:17:00 +0200

    Update main.mjs

--- backend/main.mjs
+++ backend/main.mjs
@@ -11,6 +11,15 @@ async function moveFile(input, output, cb) {
     sh.toFile(output, cb)
 }
 
+async function moveFile32(input, output, cb) {
+    const readStream = fs.createReadStream(input);
+    const sh = sharp({unlimited: true});
+
+    sh.resize({height: 32, width: 32});
+    readStream.pipe(sh);
+    sh.toFile(output, cb)
+}
+
 setInterval(async () => {
     const isRCREnabled = fs.readFileSync("C:\\RCOC\\isEnabled.rco", 'utf8') == "t";
     if (isRCREnabled && fs.existsSync("C:\\RCOC\\selected.json")) {
@@ -19,8 +28,9 @@ setInterval(async () => {
         const ver = await utils.getRobloxVersion();
         if (fs.existsSync(`${locapp}\\Roblox\\Versions`)) {
             const rv = `${locapp}\\Roblox\\Versions\\${ver}\\content\\textures\\Cursors\\KeyboardMouse`;
-            const curhashes = await utils.getMD5s({path: selected["Arrow"], name: "Arrow"}, {path: selected["ArrowFar"], name: "ArrowFar"}, {path: selected["IBeam"], name: "IBeam"});
-            const oldhashes = await utils.getMD5s({path: `${rv}\\ArrowCursor.png`, name: "Arrow"}, {path: `${rv}\\ArrowFarCursor.png`, name: "ArrowFar"}, {path: `${rv}\\IBeamCursor.png`, name: "IBeam"});
+            const r = `${locapp}\\Roblox\\Versions\\${ver}\\content\\textures\\Cursors`
+            const curhashes = await utils.getMD5s({path: selected["Arrow"], name: "Arrow"}, {path: selected["ArrowFar"], name: "ArrowFar"}, {path: selected["IBeam"], name: "IBeam"}, {path: selected["ShiftLock"], name: 'ShiftLock'});
+            const oldhashes = await utils.getMD5s({path: `${rv}\\ArrowCursor.png`, name: "Arrow"}, {path: `${rv}\\ArrowFarCursor.png`, name: "ArrowFar"}, {path: `${rv}\\IBeamCursor.png`, name: "IBeam"}, {path: `${r}\\MouseLockedCursor.png`, name: "ShiftLock"});
             if (curhashes.find((val) => val.name == "Arrow").hash != oldhashes.find((val) => val.name == "Arrow").hash) {
                 moveFile(selected["Arrow"], `${rv}\\ArrowCursor.png`, (/** @type {Error} */error, /** @type {sharp.OutputInfo}*/ info) => {})
             }
@@ -32,11 +42,16 @@ setInterval(async () => {
             if (curhashes.find((val) => val.name == "IBeam").hash != oldhashes.find((val) => val.name == "IBeam").hash) {
                 moveFile(selected["IBeam"], `${rv}\\IBeamCursor.png`, (/** @type {Error} */error, /** @type {sharp.OutputInfo}*/ info) => {})
             }
+
+            if (curhashes.find((val) => val.name == "ShiftLock").hash != oldhashes.find((val) => val.name == "ShiftLock").hash) {
+                moveFile32(selected["ShiftLock"], `${r}\\MouseLockedCursor.png`, (/** @type {Error} */error, /** @type {sharp.OutputInfo}*/ info) => {})
+            }
         }
         if (fs.existsSync("C:\\Program Files (x86)\\Roblox\\Versions")) {
             const rv = `C:\\Program Files (x86)\\Roblox\\Versions\\${ver}\\content\\textures\\Cursors\\KeyboardMouse`;
-            const curhashes = await utils.getMD5s({path: selected["Arrow"], name: "Arrow"}, {path: selected["ArrowFar"], name: "ArrowFar"}, {path: selected["IBeam"], name: "IBeam"});
-            const oldhashes = await utils.getMD5s({path: `${rv}\\ArrowCursor.png`, name: "Arrow"}, {path: `${rv}\\ArrowFarCursor.png`, name: "ArrowFar"}, {path: `${rv}\\IBeamCursor.png`, name: "IBeam"});
+            const r = `C:\\Program Files (x86)\\Roblox\\Versions\\${ver}\\content\\textures\\Cursors`
+            const curhashes = await utils.getMD5s({path: selected["Arrow"], name: "Arrow"}, {path: selected["ArrowFar"], name: "ArrowFar"}, {path: selected["IBeam"], name: "IBeam"}, {path: selected["ShiftLock"], name: 'ShiftLock'});
+            const oldhashes = await utils.getMD5s({path: `${rv}\\ArrowCursor.png`, name: "Arrow"}, {path: `${rv}\\ArrowFarCursor.png`, name: "ArrowFar"}, {path: `${rv}\\IBeamCursor.png`, name: "IBeam"}, {path: `${r}\\MouseLockedCursor.png`, name: "ShiftLock"});
             if (curhashes.find((val) => val.name == "Arrow").hash != oldhashes.find((val) => val.name == "Arrow").hash) {
                 fs.copyFileSync(selected["Arrow"], `${rv}\\ArrowCursor.png`);
                 moveFile(selected["Arrow"], `${rv}\\ArrowCursor.png`, (/** @type {Error} */error, /** @type {sharp.OutputInfo}*/ info) => {})
@@ -48,6 +63,10 @@ setInterval(async () => {
 
             if (curhashes.find((val) => val.name == "IBeam").hash != oldhashes.find((val) => val.name == "IBeam").hash) {
                 moveFile(selected["IBeam"], `${rv}\\IBeamCursor.png`, (/** @type {Error} */error, /** @type {sharp.OutputInfo}*/ info) => {})
+            }
+
+            if (curhashes.find((val) => val.name == "ShiftLock").hash != oldhashes.find((val) => val.name == "ShiftLock").hash) {
+                moveFile32(selected["ShiftLock"], `${r}\\MouseLockedCursor.png`, (/** @type {Error} */error, /** @type {sharp.OutputInfo}*/ info) => {})
             }
         }
     }
